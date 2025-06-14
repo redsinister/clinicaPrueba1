01d32a389c0b7b7685301d76852707a0cd969f93
aaron1 <i202404228@cibertec.edu.pe>
2025-06-14 10:34:58 -0500

    segundo commit

---
+++ src/test/java/org/cibertec/proy1/Proyecto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cibertec.proy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
